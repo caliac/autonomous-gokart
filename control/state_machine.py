2c73b76b97a847728886ae74a760d930cf357f4c
Calia <caliazychau@gmail.com>
2026-06-28 21:39:17 -0400

    created state architecture

---
+++ control/state_machine.py
@@ -0,0 +1,22 @@
+from robot_states import Idle
+
+class RobotStateMachine(object):
+
+    def __init__(self, robot):
+        self.robot = robot
+        self.state = Idle()
+        self.state.enter(robot)
+
+    def on_event(self, event):
+        next_state = self.state.on_event(event)
+
+        if next_state.__class__ != self.state.__class__:
+            print(f"{self.state} is transitioning to {next_state}")
+            self.state.end(self.robot)
+            self.state = next_state
+            self.state.enter(self.robot)
+
+
+
+    def update(self):
+        self.state.execute(self.robot)
