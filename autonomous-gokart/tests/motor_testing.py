b7aba9d6ba0715b2f004664648e084bf48bf444b
Calia <caliazychau@gmail.com>
2026-07-25 14:58:41 -0400

    change enable to be always on, fix print lines

--- autonomous-gokart/tests/motor_testing.py
+++ autonomous-gokart/tests/motor_testing.py
@@ -8,7 +8,7 @@ class Driver:
         self.R_EN = 23
         self.L_EN = 24
 
-        self.freq = 20000 #PWM frequency set to 20kHz
+        self.freq = 1000
         
 
         self.handle = GPIO.gpiochip_open(4) #raspberry pi 5 uses handle 4
@@ -19,11 +19,15 @@ class Driver:
         GPIO.gpio_claim_output(self.handle, self.L_EN)
         GPIO.gpio_claim_output(self.handle, self.LPWM)
 
-        #make sure everything is off
-        GPIO.gpio_write(self.handle, self.R_EN, 0)
-        GPIO.gpio_write(self.handle, self.L_EN, 0)
+        #enable motor driver
+        GPIO.gpio_write(self.handle, self.R_EN, 1)
+        GPIO.gpio_write(self.handle, self.L_EN, 1)
+
+        #start with motor stopped
         GPIO.tx_pwm(self.handle, self.RPWM, self.freq, 0)
         GPIO.tx_pwm(self.handle, self.LPWM, self.freq, 0)
+
+        print("Motor driver initialized")
 
 
     def set_speed(self, speed):
@@ -32,28 +36,19 @@ class Driver:
 
         if speed > 0: #clockwise
             GPIO.tx_pwm(self.handle, self.LPWM, self.freq, 0) #stop left PWM
-            GPIO.gpio_write(self.handle, self.L_EN, 0) #set left_enable to 0V
-
-            GPIO.gpio_write(self.handle, self.R_EN, 1) #send voltage to right_enable
             GPIO.tx_pwm(self.handle, self.RPWM, self.freq, speed) #begin right PWM
 
-            print("Spinning clockwise at: {speed}%.")
+            print(f"Spinning clockwise at {speed}% speed.")
 
         elif speed < 0: #counterclockwise
             ccspeed = abs(speed)
             GPIO.tx_pwm(self.handle, self.RPWM, self.freq, 0) #stop right PWM
-            GPIO.gpio_write(self.handle, self.R_EN, 0) #set right_enable to 0V
-
-            GPIO.gpio_write(self.handle, self.L_EN, 1) #send voltage to left_enable
             GPIO.tx_pwm(self.handle, self.LPWM, self.freq, ccspeed) #begin left PWM
 
-            print("Spinning counter-clockwise at: {ccspeed}%.")
+            print(f"Spinning counter-clockwise at {ccspeed}% speed.")
 
         else: #else, speed is 0 -> stops motor
-            #sets voltages to 0
-            GPIO.gpio_write(self.handle, self.R_EN, 0)
-            GPIO.gpio_write(self.handle, self.L_EN, 0)
-            
+
             #parameters of tx_pwm: handle, GPIO pin, frequency, duty cycles (0 = on HIGH 0% of the time)
             GPIO.tx_pwm(self.handle, self.RPWM, self.freq, 0)
             GPIO.tx_pwm(self.handle, self.LPWM, self.freq, 0)
@@ -61,11 +56,14 @@ class Driver:
 
 
     def end(self):
+        print("Shutting down motor")
         GPIO.tx_pwm(self.handle, self.RPWM, self.freq, 0)
         GPIO.tx_pwm(self.handle, self.LPWM, self.freq, 0)
         GPIO.gpio_write(self.handle, self.R_EN, 0)
         GPIO.gpio_write(self.handle, self.L_EN, 0)
         GPIO.gpiochip_close(self.handle)
+
+        print("Cleanup complete")
 
 my_driver = Driver()
 
@@ -85,6 +83,7 @@ try:
 
 except KeyboardInterrupt:
     my_driver.set_speed(0)
+    print("Interrupted by user")
 
 finally:
     my_driver.end()
