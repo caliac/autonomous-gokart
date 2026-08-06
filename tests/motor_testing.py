1acd0a348b85778c74f88270dbe05545b6319ff0
Calia <caliazychau@gmail.com>
2026-08-05 21:40:02 -0400

    add speed values for driver2

--- tests/motor_testing.py
+++ tests/motor_testing.py
@@ -1,4 +1,4 @@
-import lgpio as GPIO
+#import lgpio as GPIO
 import time
 from hardware.motor_driver import MotorDriver
 
@@ -10,28 +10,37 @@ try:
     print("Starting motor test.")
 
     driver1.set_speed(30)
+    driver2.set_speed(30)
     time.sleep(2)
 
     driver1.set_speed(80)
+    driver2.set_speed(80)
     time.sleep(4)
 
     driver1.set_speed(0)
+    driver2.set_speed(0)
     time.sleep(1)
 
     driver1.set_speed(-30)
+    driver2.set_speed(-30)
     time.sleep(2)
 
     driver1.set_speed(-100)
+    driver2.set_speed(-100)
     time.sleep(3)
 
     driver1.set_speed(-50)
+    driver2.set_speed(-50)
     time.sleep(2)
 
     driver1.set_speed(0)
+    driver2.set_speed(0)
 
 except KeyboardInterrupt:
     driver1.set_speed(0)
+    driver2.set_speed(0)
     print("Interrupted by user")
 
 finally:
     driver1.end()
+    driver2.end()
