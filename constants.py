5e407c3915cf1b0b46404145ed1eefb3fc267245
Calia <caliazychau@gmail.com>
2026-08-05 20:58:30 -0400

    fix nested repo issue

---
+++ constants.py
@@ -0,0 +1,6 @@
+#constants/variables go here (ex. P = xxx, I=xxx, D=xxx)
+
+#reference these variables by doing constants.variable_name
+
+height = 2592 #height of camera image
+width = 4608 #width of camera image
