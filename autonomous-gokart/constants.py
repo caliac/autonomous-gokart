02980c0531fb453cb403a4499807b83428310527
Calia <caliazychau@gmail.com>
2026-08-05 21:08:27 -0400

    ACTUALLY fix nested repo issue

--- autonomous-gokart/constants.py
+++
@@ -1,6 +0,0 @@
-#constants/variables go here (ex. P = xxx, I=xxx, D=xxx)
-
-#reference these variables by doing constants.variable_name
-
-height = 2592 #height of camera image
-width = 4608 #width of camera image
