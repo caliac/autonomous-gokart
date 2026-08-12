57908d3bb0c3a450f32b5adfb70f16bd8a55b841
Calia <caliazychau@gmail.com>
2026-08-11 21:39:15 -0400

    driving logic + combine forward&backward->drive

--- control/wheels.py
+++ control/wheels.py
@@ -6,20 +6,19 @@ class Wheels:
     def __init__(self, left_motor, right_motor): #motor 1(driver1(pin1, pin2, pin3, pin4)), motor 2(driver2(pin1, pin2, pin3, pin4))
         self.left_motor = left_motor
         self.right_motor = right_motor
+        self.turning_constant = 10
 
-    def forward(self, speed):
+    def drive(self, speed):
         self.left_motor.set_speed(speed)
         self.right_motor.set_speed(speed)
 
-    def backward(self, speed):
-        pass
-        #maybe combine forward and backward into just drive(self, speed)
-
     def left(self, speed):
-        pass
+        self.left_motor.set_speed(self.turning_constant)
+        self.right_motor.set_speed(speed)
 
     def right(self, speed):
-        pass
+        self.left_motor.set_speed(speed)
+        self.right_motor.set_speed(self.turning_constant)
 
     def brake(self):
         self.left_motor.set_speed(0)
