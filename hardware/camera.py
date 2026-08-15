a33640e06100fdf19a20a5661196fda7b2273bd4
Calia <caliazychau@gmail.com>
2026-08-15 18:44:31 -0400

    fix camera class, add transform to rotate 180

--- hardware/camera.py
+++ hardware/camera.py
@@ -1,14 +1,21 @@
 #camera class
 
 import time
+import libcamera
 from picamera2 import Picamera2
 
 cam = Picamera2()
+
+config = cam.create_preview_configuration(
+    transform=libcamera.Transform(hflip=1, vflip=1)
+)
+cam.configure(config)
+
 cam.start()
 
 time.sleep(2)
 
-cam.capture_file("8-15-26 photo")
+cam.capture_file("myPhoto3.jpg")
 print("image captured.")
 
 cam.close()
