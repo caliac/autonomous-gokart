7d9a32aaa789dbc0b4605e442f728908da1d2f78
Calia <caliazychau@gmail.com>
2026-08-15 18:30:12 -0400

    basic camera code for taking a picture

--- hardware/camera.py
+++ hardware/camera.py
@@ -1,1 +1,14 @@
 #camera class
+
+import time
+from picamera2 import Picamera2
+
+cam = Picamera2()
+cam.start()
+
+time.sleep(2)
+
+cam.capture_file("8-15-26 photo")
+print("image captured.")
+
+cam.close()
